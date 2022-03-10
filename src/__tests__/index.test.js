fe1eaf8066a0b9baebb7e666e22989f99ca0ceac
Matheus Wichman <matheushw@outlook.com>
2022-03-09 23:19:47 -0300

    demo for missing act warning

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,16 @@
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
+import jsdomGlobal from 'jsdom-global'
 import Counter from '../'
+
+jsdomGlobal()
 
 test('increments the count', () => {
   const {getByText} = render(<Counter />)
-  const button = getByText('0')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('1')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('2')
+  // const button = getByText('0')
+  // fireEvent.click(button)
+  // expect(button).toHaveTextContent('1')
+  // fireEvent.click(button)
+  // expect(button).toHaveTextContent('2')
 })
-
